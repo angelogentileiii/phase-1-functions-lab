127113f4f50c7410c6407d47435480ebf24b8770
Angelo Gentile III <angelo.john.gentile@gmail.com>
2023-10-02 18:55:25 -0400

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(pickup, headquarters = 42){
+    return Math.abs(pickup - headquarters);
+};
+
+function distanceFromHqInFeet(blocks){
+    return distanceFromHqInBlocks(blocks) * 264;
+};
+
+function distanceTravelledInFeet(start, end){
+    return Math.abs((end - start)*264);
+}
+
+function calculatesFarePrice(start, end){
+    let distance = distanceTravelledInFeet(start, end)
+    let fare;
+    if (distance <= 400) {fare = 0}
+    else if (distance <= 2000) {fare = (distance - 400) * .02}
+    else if (distance <= 2500) {fare = 25}
+    else if (distance > 2500) {fare = 'cannot travel that far'};
+    return fare;
+};
+
